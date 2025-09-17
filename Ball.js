0dcb7cf4555b7def666dfa31f605935083c1be44
Ammel <77129284+Ammelll@users.noreply.github.com>
2025-09-17 18:10:31 +0000

    improving ball collision

--- Ball.js
+++ Ball.js
@@ -10,23 +10,23 @@ class Ball{
 
         let curve = Ball.getBezierCurve(this.pos.x);
 
-        if(curve != undefined){
-            const t = Ball.getTForX(curve,this.pos.x)
-            const y = bezierPoint(curve[0].y,curve[1].y,curve[2].y,curve[3].y,t);
-            //3{(1−t)2(P1−P0)+2t(1−t)(P2−P1)+t2(P3−P2)}
-            const slope = -1*3*((1-t)**2*(curve[1].y-curve[0].y)+2*t*(1-t)*(curve[2].y-curve[1].y)+t**2*(curve[3].y-curve[2].y)); 
-            if (this.pos.y-12.5 > y) {
-                this.pos.y = y-12.5;
-            }
-            this.pos.y = this.pos.y-12.5>y ? y-12.5 : this.pos.y;
-            //B″(t) = 6(1-t)(P₂ - 2P₁ + P₀) + 6t(P₃ - 2P₂ + P₁) = 6[ (1-t)(P₂ - 2P₁ + P₀) + t(P₃ - 2P₂ + P₁) ]
-            const slopePrime = 6*(1-t)*(curve[2].y-2*curve[1].y+curve[0].y) + 6*t*(curve[3].y-2*curve[2].y + curve[1].y);
-            if(slopePrime !=0 && Math.abs(slope-previousSlope) > 100 && Math.abs(this.pos.y - y) <= 25){
-                this.velocity.y = -10;
-            }
-            previousSlope = slope;
+        if(curve == undefined){
+            return circle(this.pos.x,this.pos.y,25);
 
         }
+        const t = Ball.getTForX(curve,this.pos.x)
+        const y = bezierPoint(curve[0].y,curve[1].y,curve[2].y,curve[3].y,t);
+        //3{(1−t)2(P1−P0)+2t(1−t)(P2−P1)+t2(P3−P2)}
+        const slope = -1*3*((1-t)**2*(curve[1].y-curve[0].y)+2*t*(1-t)*(curve[2].y-curve[1].y)+t**2*(curve[3].y-curve[2].y)); 
+
+        this.pos.y = this.pos.y+12.5 > y ? y-12.5 : this.pos.y;
+        //B″(t) = 6(1-t)(P₂ - 2P₁ + P₀) + 6t(P₃ - 2P₂ + P₁) = 6[ (1-t)(P₂ - 2P₁ + P₀) + t(P₃ - 2P₂ + P₁) ]
+        const slopePrime = 6*(1-t)*(curve[2].y-2*curve[1].y+curve[0].y) + 6*t*(curve[3].y-2*curve[2].y + curve[1].y);
+        if(slopePrime !=0 && Math.abs(slope-previousSlope) > 100 && Math.abs(this.pos.y - y) <= 25){
+            this.velocity.y = -10;
+        }
+        previousSlope = slope;
+
 
         circle(this.pos.x,this.pos.y,25);
     }
